4b9eec2a8d2f862dfcfe482c681f381ab8076ca4
marseous <59691308+marseous@users.noreply.github.com>
2020-12-12 15:53:37 +0300

    Created Tank class and Tank Controller class

---
+++ TankGame/Source/TankGame/TankPlayerController.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	FString tankName = GetWorld()->GetFirstPlayerController()->GetPawn()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("GETCONTROLLEDTANK : %s"), *tankName);
+
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("Tank Player Controller: Begin Play!"));
+
+
+}
