443171553d719d3ce1d1cc3633c064caf3d02f2d
marseous <59691308+marseous@users.noreply.github.com>
2020-12-15 01:49:57 +0300

    Barrel changed on C++ class

---
+++ TankGame/Source/TankGame/TankBarrel.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TankGame.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankBarrel.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta = (BlueprintSpawnableComponent), hidecategories = ("Collision"))
+class TANKGAME_API UTankBarrel : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+public:
+
+	void Elevate(float Degree);
+
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	float MaxDegreesPerSecond = 20.f;
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	float MinDegreesPerSecond = 0.f;
+	UPROPERTY(EditAnywhere, Category = "Setup")
+	float MaxElevetionDegrees = 40.f;
+};
